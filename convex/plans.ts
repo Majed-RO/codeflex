2ad72b8f29f0cb970aa029e0e5af626550395f45
Webdev Majed <webdev.majed@gmail.com>
2025-08-29 11:22:15 +0300

    first publish

---
+++ convex/plans.ts
@@ -0,0 +1,69 @@
+import { v } from 'convex/values';
+import { mutation, query } from './_generated/server';
+
+export const createPlan = mutation({
+	args: {
+		userId: v.string(),
+		name: v.string(),
+		workoutPlan: v.object({
+			schedule: v.array(v.string()),
+			exercises: v.array(
+				v.object({
+					day: v.string(),
+					routines: v.array(
+						v.object({
+							name: v.string(),
+							sets: v.number(),
+							reps: v.number()
+						})
+					)
+				})
+			)
+		}),
+		dietPlan: v.object({
+			dailyCalories: v.number(),
+			meals: v.array(
+				v.object({
+					name: v.string(),
+					foods: v.array(v.string())
+				})
+			)
+		}),
+		isActive: v.boolean()
+	},
+	handler: async (ctx, args) => {
+		// before we create the new plan, we have to de-activate the last one, so ONE plan would be active
+
+		const activePlans = await ctx.db
+			.query('plans')
+			.withIndex('by_user_id', q =>
+				q.eq('userId', args.userId)
+			)
+			.filter(q => q.eq(q.field('isActive'), true))
+			.collect();
+
+		// deactivate all active plans for the user
+		for (const plan of activePlans) {
+			await ctx.db.patch(plan._id, { isActive: false });
+		}
+
+		const planId = await ctx.db.insert('plans', args);
+
+		return planId;
+	}
+});
+
+export const getUserPlans = query({
+	args: { userId: v.string() },
+	handler: async (ctx, args) => {
+		const plans = await ctx.db
+			.query('plans')
+			.withIndex('by_user_id', q =>
+				q.eq('userId', args.userId)
+			)
+			.order('desc')
+			.collect();
+
+		return plans;
+	}
+});
